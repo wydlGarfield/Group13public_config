0676cf23b43f12aeb0386396fb3e3f5b7b5de257
wydlGarfield <41653898+wydlGarfield@users.noreply.github.com>
2018-10-11 15:23:47 +1300

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+sudo cp -a /var/www/html/wordpress/. /var/www/html
+
+#salts
+
+perl -i -pe'
+  BEGIN {
+    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+  }
+  s/put your unique phrase here/salt()/ge
+' /var/www/html/wp-config.php
+
+
+chown ubuntu:www-data  -R /var/www/html/* 
+find /var/www/html/ -type d -exec chmod 755 {} \;  # Change directory permissions rwxr-xr-x
+find /var/www/html/ -type f -exec chmod 644 {} \;
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+cd ..
+rm -rf temp
+mv /var/www/html/index.html /var/www/html/apachedefault.html
+
 ipurl="$(curl ipinfo.io/ip --max-time 8)"  
 siteurl=${ipurl}
 adminuser=wordpress
